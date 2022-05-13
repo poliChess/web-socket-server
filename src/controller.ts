c1283ce2d04bcd91b1782bf6089aa61eb309a7bf
mihneabuz <mihneabuzatu88@gmail.com>
2022-05-13 18:34:48 +0300

    changed route and added check for socket connections

--- src/controller.ts
+++ src/controller.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 
+const delay = (milis) => new Promise(resolve => setTimeout(resolve, milis));
+
 const newMatchInfo = (matchID: string, player1ID: string, player2ID: string) => ({
   matchID,
   player1ID,
@@ -17,30 +19,33 @@ function ControllerEndpoint(sockets, matches) {
     res.send('yes');
   });
 
-  app.post('/startGame', (req, res) => {
+  app.post('/startGame', async (req, res) => {
     const matchID = req.body.matchID;
     const player1ID = req.body.player1ID;
     const player2ID = req.body.player2ID;
+    const player1Username = req.body.player1Username;
+    const player2Username = req.body.player2Username;
 
-    if (!matchID || !player1ID || !player2ID)
+    if (!matchID || !player1ID || !player2ID || !player1Username || !player2Username)
       return res.status(400).send('bad request');
 
-    // TODO: check if both players opened a socket
+    while (!sockets[player1ID])
+      await delay(300);
+
+    if (player2ID != 'computer') {
+      while (!sockets[player2ID])
+        await delay(300);
+    }
 
     const match = newMatchInfo(matchID, player1ID, player2ID);
 
-    matches[player1ID] = match;
-    if (match.toMove)
-      sockets[player1ID].send('start second');
-    else
-      sockets[player1ID].send('start first');
+    console.log('starting game: ' + match);
 
+    matches[player1ID] = match;
+    sockets[player1ID].send(`start first vs ${player2Username}`);
     if (player2ID != 'computer') {
       matches[player2ID] = match;
-      if (match.toMove)
-        sockets[player1ID].send('start first');
-      else
-        sockets[player1ID].send('start second');
+      sockets[player2ID].send(`start second vs ${player1Username}`);
     }
 
     return res.send('ok');
